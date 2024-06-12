04f1233e4a65ff2f110ee9794d98af467da193f6
TanushKrashak <tankrashak38816@gmail.com>
2024-06-12 15:03:27 +0530

    Created Interface For Interacting

---
+++ Source/Cpp_Test_CTF/Private/Interfaces/Cpp_InteractionInterface.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Cpp_InteractionInterface.h"
+
+// Add default functionality here for any ICpp_InteractionInterface functions that are not pure virtual.
+
+void ICpp_InteractionInterface::OnProjectileHit(AActor* OtherActor, UPrimitiveComponent* OtherComponent, const FHitResult& HitResult) {
+
+}
---
+++ Source/Cpp_Test_CTF/Public/Interfaces/Cpp_InteractionInterface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Cpp_InteractionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UCpp_InteractionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class CPP_TEST_CTF_API ICpp_InteractionInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	// Func for when projectile hits something
+	UFUNCTION(Category = "Interaction")
+	virtual void OnProjectileHit(AActor* OtherActor, UPrimitiveComponent* OtherComponent, const FHitResult& HitResult);
+};
