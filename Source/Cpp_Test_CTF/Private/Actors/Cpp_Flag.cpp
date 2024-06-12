fe8672b5ba67d407023d5612d58e67f7a9f405fc
TanushKrashak <tankrashak38816@gmail.com>
2024-06-12 17:44:26 +0530

    Added Flag Class

---
+++ Source/Cpp_Test_CTF/Private/Actors/Cpp_Flag.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Cpp_Flag.h"
+
+ACpp_Flag::ACpp_Flag() {
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void ACpp_Flag::BeginPlay() {
+	Super::BeginPlay();
+
+	
+}
+
